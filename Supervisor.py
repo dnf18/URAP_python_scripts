978772ea65997661ac6daf0639fb9ad36abeba7b
dnf18 <146392135+dnf18@users.noreply.github.com>
2026-02-02 21:01:44 -0800

    Supervisor.py

--- Supervisor.py
+++ Supervisor.py
@@ -12,18 +12,16 @@ class Supervisor:
 
     def run(self):
 
-        # -------------------------------
         # Output directories
-        # -------------------------------
+
         run_ref = self.dir / "run_ref"
         run_test = self.dir / "run_test"
 
         run_ref.mkdir(parents=True, exist_ok=True)
         run_test.mkdir(parents=True, exist_ok=True)
 
-        # -------------------------------
         # Get steering config
-        # -------------------------------
+
         steering = Steering.user_input()
 
         json_ref  = run_ref  / "steering_config.json"
@@ -32,15 +30,13 @@ class Supervisor:
         steering.save(json_ref)
         steering.save(json_test)
 
-        # -------------------------------
         # Run DataFlow
-        # -------------------------------
+
         DataFlow(str(json_ref)).run_full_pipeline()
         DataFlow(str(json_test)).run_full_pipeline()
 
-        # -------------------------------
         # Histogram paths
-        # -------------------------------
+     
         ref_hist = run_ref / "results" / "energy_hist.json"
         test_hist = run_test / "results" / "energy_hist.json"
 
@@ -48,9 +44,19 @@ class Supervisor:
         histogram_dir = self.dir / "histograms"
         histogram_dir.mkdir(exist_ok=True)
 
-        # -------------------------------
+        # Spectrum paths
+
+        ref_meta = run_ref / "results" / "spectrum_meta.json"
+        test_meta = run_test / "results" / "spectrum_meta.json"
+
+        with open(ref_meta) as f:
+            ref_png = json.load(f)["spectrum_png"]
+
+        with open(test_meta) as f:
+            test_png = json.load(f)["spectrum_png"]
+
         # Run Comparator
-        # -------------------------------
+      
         comp = Comparator(
             ref_json=str(ref_hist),
             test_json=str(test_hist),
@@ -62,13 +68,16 @@ class Supervisor:
         results = comp.compare()
         overlay_plot = comp.plot_overlay()
 
-        # -------------------------------
         # Reporter Output
-        # -------------------------------
+   
         reporter = Reporter(config_json=str(json_ref))
         reporter.generate_pdf(
             results=results,
-            histograms={"Energy Comparison": overlay_plot}
+            histograms={
+                "Reference Spectrum": ref_png,
+                "Test Spectrum": test_png,
+                "Overlay Comparison": overlay_plot
+            }
         )
 
         print("\n[Supervisor] COMPLETED SUCCESSFULLY\n")
